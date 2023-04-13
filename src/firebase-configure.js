ec5e81f683cd5f43a3ecedb8be7aa055da4dfd8d
c-19 <92860234+nathanaelmehari@users.noreply.github.com>
2023-04-13 03:58:26 +0300

    Update firebase-configure.js

--- src/firebase-configure.js
+++ src/firebase-configure.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyCh8u-DLMS14srrBZrqj1amDq45tG22YWY",
-  authDomain: "newn-d2ee2.firebaseapp.com",
-  projectId: "newn-d2ee2",
-  storageBucket: "newn-d2ee2.appspot.com",
-  messagingSenderId: "244799206766",
-  appId: "1:244799206766:web:9cf0f36455db07605b7d7f"
+  copy the code from firebase.com
+  //apiKey: "",
+  //authDomain: "",
+  //projectId: "",
+  ///storageBucket: "",
+  //messagingSenderId: "",
+  //appId: ""
 };
 
 
